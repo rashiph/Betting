ba9f3f19f7ba60d3ee88d2dd5ee07418f7110535
Rashi Phuloria <rashiph@thoughtworks.com>
2015-02-26 21:39:12 +0530

    Took out the dividend calculation logic in service.

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 ## First time setup
 
+Run `npm install`.
 Run `npm install --global bower grunt-cli` to install all dependencies.
 Run `bower install` to download bower components.
 
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,16 +1,22 @@
 'use strict';
 
 angular.module('tabcorpApp')
-  .controller('MainCtrl', function ($scope, localStorageService, Bet, Result, Products) {
+  .controller('MainCtrl', function ($scope, localStorageService, dividendService, Bet, Result) {
     var betsInStore = localStorageService.get('bets');
 
     $scope.bets = betsInStore || [];
+    $scope.error = '';
 
     $scope.$watch('bets', function () {
       localStorageService.set('bets', $scope.bets);
     }, true);
 
     $scope.addBet = function () {
+      $scope.error = '';
+      if(!verifyBet()){
+        $scope.error = 'Error : Invalid Bet Placed';
+        return;
+      }
       var bet = new Bet($scope.bet);
       $scope.bets.push(bet);
       $scope.bet = '';
@@ -21,73 +27,22 @@ angular.module('tabcorpApp')
     };
 
     $scope.declareResult = function () {
-      var result = new Result($scope.result);
-      generateDividend(result);
-    };
-
-    var generateDividend = function (result) {
-      calculateDividendOfWinPool(result);
-      calculateDividendOfPlacePool(result);
-      calculateDividendOfExactaPool(result);
-      calculateDividendOfQuinellaPool(result);
-    };
-
-    var calculateDividendOfWinPool = function (result) {
-      var winPool = getPoolBy(Products.WIN);
-      var total = totalStakeAmount(winPool);
-      var commission = ((total * 15 ) / 100).toFixed(2);
-
-      $scope.dividendOfWinPool = calculateDividend(winPool, [result.firstRunnerUp], total - commission);
-    };
-
-    var calculateDividendOfPlacePool = function (result) {
-      var placePool = getPoolBy(Products.PLACE);
-      var total = totalStakeAmount(placePool);
-      var commission = ((total * 12 ) / 100).toFixed(2);
-      var splitAmount = ((total - commission ) / 3).toFixed(2);
-
-      $scope.dividendOfPlacePoolForFirstRunnerUp = calculateDividend(placePool, [result.firstRunnerUp], splitAmount);
-      $scope.dividendOfPlacePoolForSecondRunnerUp = calculateDividend(placePool, [result.secondRunnerUp], splitAmount);
-      $scope.dividendOfPlacePoolForThirdRunnerUp = calculateDividend(placePool, [result.thirdRunnerUp], splitAmount);
-    };
-
-    var calculateDividendOfExactaPool = function (result) {
-      var exactaPool = getPoolBy(Products.EXACTA);
-      var total = totalStakeAmount(exactaPool);
-      var commission = ((total * 18 ) / 100).toFixed(2);
-
-      $scope.dividendOfExactaPool = calculateDividend(exactaPool, [result.firstRunnerUp + ',' + result.secondRunnerUp], total - commission);
-    };
-
-    var calculateDividendOfQuinellaPool = function (result) {
-      var quinellaPool = getPoolBy(Products.QUINELLA);
-      var total = totalStakeAmount(quinellaPool);
-      var commission = ((total * 18 ) / 100).toFixed(2);
-
-      $scope.dividendOfQuinellaPool = calculateDividend(quinellaPool, [result.firstRunnerUp + ',' + result.secondRunnerUp, result.secondRunnerUp + ',' + result.firstRunnerUp], total - commission);
-    };
-
-    var calculateDividend = function (pool, runners, poolCollection) {
-      var winningBets = _.filter(pool, function (bet) {
-        return _.contains(runners, bet.selection);
-      });
-
-      var totalStakeOnWinningHorse = totalStakeAmount(winningBets);
-      if (totalStakeOnWinningHorse === 0) {
-        return 0;
+      $scope.error = '';
+      if(!verifyResult()){
+        $scope.error = 'Error : Invalid Result';
+        return;
       }
-      return (poolCollection / totalStakeOnWinningHorse).toFixed(2);
+      $scope.publishedResult = new Result($scope.result);
+      $scope.dividend = dividendService.calculate($scope.bets, $scope.publishedResult);
     };
 
-    var totalStakeAmount = function (pool) {
-      return _.reduce(pool, function (sum, bet) {
-        return sum + parseInt(bet.stake);
-      }, 0);
+    var verifyResult = function(){
+      var expression = /(R|r):(\d:){2,}\d/i;
+      return expression.test($scope.result)
     };
 
-    var getPoolBy = function (product) {
-      return _.filter($scope.bets, function (bet) {
-        return bet.product === product;
-      });
+    var verifyBet = function(){
+      var expression = /(W|w|E|e|P|p|Q|q):((\d,\d:)|\d:)\d/i;
+      return expression.test($scope.bet)
     };
   });
